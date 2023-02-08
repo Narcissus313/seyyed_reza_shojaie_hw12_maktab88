aa4da1b39ce32b196df3ee7147edc40c084fba96
narcissus313 <srz.shl@gmail.com>
2023-02-08 20:37:10 +0330

    2_1 started

--- 1_2/index.js
+++ 1_2/index.js
@@ -1,12 +1,8 @@
-var toPdf = require("custom-soffice-to-pdf");
-var fs = require("fs");
-var wordBuffer = fs.readFileSync("./test.docx");
+const docxConverter = require("docx-pdf");
 
-toPdf(wordBuffer).then(
-	(pdfBuffer) => {
-		fs.writeFileSync("./test.pdf", pdfBuffer);
-	},
-	(err) => {
+docxConverter("./test.docx", "./output.pdf", function (err, result) {
+	if (err) {
 		console.log(err);
 	}
-);
+	console.log("result" + result);
+});
